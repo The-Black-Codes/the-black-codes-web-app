b7087ea27cefe452aa6e95019c4b1e4f57e1869c
Austin Cumberlander <austincumberlander@gmail.com>
2024-04-18 11:23:01 -0500

    Implemented the hero slider component.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,14 @@
 import AboutUs from './components/AboutUs/AboutUs';
 import './app.css';
+import Hero from './components/Hero/Hero';
 
-export function App() {
+export const App = () => {
   return (
     <div className="primary-bg mx-auto max-w-custom">
+      <Hero />
       <AboutUs />
     </div>
   );
-}
+};
 
 export default App;
