f116c029f5a2ef80ec3919c400882979df93fa57
Austin Cumberlander <austincumberlander@gmail.com>
2024-04-23 09:13:49 -0500

    Mobile view that includes hamburger menu is complete.

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,21 +1,44 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './Navbar.css';
+import MobileView from './Views/MobileView';
 
 const Navbar = () => {
+  const [mobileView, setMobileView] = useState(false);
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 768
+        ? setMobileView(true)
+        : setMobileView(false);
+    };
+
+    setResponsiveness();
+
+    window.addEventListener('resize', () => setResponsiveness());
+  }, []);
+
+  const view = mobileView ? 'hamburger' : 'navbar-container';
+
   return (
-    <div className="navbar-container flex justify-evenly p-1 mt-12">
-      <div>
-        <a href="#home">Home</a>
-      </div>
-      <div>
-        <a href="#about-us">About</a>
-      </div>
-      <div>
-        <a href="#contact-us">Contact Us</a>
-      </div>
-      <div>
-        <a href="#donate">Donate</a>
-      </div>
+    <div className={view}>
+      {mobileView ? (
+        <MobileView />
+      ) : (
+        <>
+          <div>
+            <a href="#home">Home</a>
+          </div>
+          <div>
+            <a href="#about-us">About</a>
+          </div>
+          <div>
+            <a href="#contact-us">Contact Us</a>
+          </div>
+          <div>
+            <a href="#donate">Donate</a>
+          </div>
+        </>
+      )}
     </div>
   );
 };
