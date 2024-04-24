2e0f922923ead9224a971b3292cd5ff5597ca41b
Austin Cumberlander <austincumberlander@gmail.com>
2024-04-24 17:51:57 -0500

    Component is now mobile responsive.

---
+++ src/app/components/Milestones/Views/MobileView.tsx
@@ -0,0 +1,132 @@
+import './MobileView.css';
+
+const MobileView = () => {
+  return (
+    <div className="mobile-timeline-container">
+      <div className="year-bubble-container">
+        <div className="mobile-year-bubble">
+          <h3 className="text-white font-bold uppercase secondary-header-font">
+            2017
+          </h3>
+        </div>
+      </div>
+
+      {/* Trailing Bubbles */}
+      <div className="trailing-bubble-1"></div>
+      <div className="trailing-bubble-2"></div>
+
+      <div className="milestone-blurb">
+        <h2 className="text-white font-bold uppercase secondary-header-font">
+          FOUNDED
+        </h2>
+        <p className="text-white primary-font">
+          A blend of various influences and shaped by the historical experiences
+          of black people in the country.
+        </p>
+      </div>
+
+      {/* Trailing Bubbles */}
+      <div className="trailing-bubble-2"></div>
+      <div className="trailing-bubble-1"></div>
+
+      <div className="year-bubble-container">
+        <div className="mobile-year-bubble">
+          <h3 className="text-white font-bold uppercase secondary-header-font">
+            2019
+          </h3>
+        </div>
+      </div>
+
+      {/* Trailing Bubbles */}
+      <div className="trailing-bubble-1"></div>
+      <div className="trailing-bubble-2"></div>
+
+      <div className="milestone-blurb">
+        <h2 className="text-white font-bold uppercase secondary-header-font">
+          1ST HACKATHON
+        </h2>
+        <p className="text-white primary-font">
+          The Black Codes held their first Hackathon at Kindful.
+        </p>
+      </div>
+
+      {/* Trailing Bubbles */}
+      <div className="trailing-bubble-2"></div>
+      <div className="trailing-bubble-1"></div>
+
+      <div className="year-bubble-container">
+        <div className="mobile-year-bubble">
+          <h3 className="text-white font-bold uppercase secondary-header-font">
+            2022
+          </h3>
+        </div>
+      </div>
+
+      {/* Trailing Bubbles */}
+      <div className="trailing-bubble-1"></div>
+      <div className="trailing-bubble-2"></div>
+
+      <div className="milestone-blurb">
+        <h2 className="text-white font-bold uppercase secondary-header-font">
+          PANEL DISCUSSION
+        </h2>
+        <p className="text-white primary-font">
+          On October 6th, 2022 The Black Codes held their first discussion panel
+          on the growth of black tech in Tennessee.
+        </p>
+      </div>
+
+      {/* Trailing Bubbles */}
+      <div className="trailing-bubble-2"></div>
+      <div className="trailing-bubble-1"></div>
+
+      <div className="year-bubble-container">
+        <div className="mobile-year-bubble">
+          <h3 className="text-white font-bold uppercase secondary-header-font">
+            2023
+          </h3>
+        </div>
+      </div>
+
+      {/* Trailing Bubbles */}
+      <div className="trailing-bubble-1"></div>
+      <div className="trailing-bubble-2"></div>
+
+      <div className="milestone-blurb">
+        <h2 className="text-white font-bold uppercase secondary-header-font">
+          INCORPORATED
+        </h2>
+        <p className="text-white primary-font">
+          The Black Codes became a 501(c)(3) organization on May 5th, 2023.
+        </p>
+      </div>
+
+      {/* Trailing Bubbles */}
+      <div className="trailing-bubble-2"></div>
+      <div className="trailing-bubble-1"></div>
+
+      <div className="year-bubble-container">
+        <div className="mobile-year-bubble">
+          <h3 className="text-white font-bold uppercase secondary-header-font">
+            2024
+          </h3>
+        </div>
+      </div>
+
+      {/* Trailing Bubbles */}
+      <div className="trailing-bubble-1"></div>
+      <div className="trailing-bubble-2"></div>
+
+      <div className="milestone-blurb">
+        <h2 className="text-white font-bold uppercase secondary-header-font">
+          LAUNCH OF NEW WEBSITE
+        </h2>
+        <p className="text-white primary-font">
+          The Black Codes launched their new website for the public.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MobileView;
