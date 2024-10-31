c18b694367632b2bdf23b5ecf573af8fca159290
Austin Cumberlander <austincumberlander@gmail.com>
2024-10-30 19:08:52 -0500

    updates

--- src/app/components/EventCalendar/EventCalendar.tsx
+++ src/app/components/EventCalendar/EventCalendar.tsx
@@ -8,26 +8,26 @@ const myEventsList = [
   {
     title: 'Hackathon',
     allDay: false,
-    start: new Date(2024, 4, 4, 10, 0),
-    end: new Date(2024, 4, 4, 11, 0),
+    start: new Date(2024, 10, 11, 10, 0),
+    end: new Date(2024, 10, 11, 11, 0),
   },
   {
     title: 'Panel Discussion',
     allDay: false,
-    start: new Date(2024, 4, 11, 11, 0),
-    end: new Date(2024, 4, 11, 13, 0),
+    start: new Date(2024, 10, 14, 10, 0),
+    end: new Date(2024, 10, 14, 11, 0),
   },
   {
     title: 'Tech Brunch',
     allDay: false,
-    start: new Date(2024, 4, 15, 10, 0),
-    end: new Date(2024, 4, 15, 12, 0),
+    start: new Date(2024, 10, 15, 10, 0),
+    end: new Date(2024, 10, 15, 11, 0),
   },
   {
     title: 'Dinner',
     allDay: false,
-    start: new Date(2024, 4, 23, 17, 0),
-    end: new Date(2024, 4, 23, 20, 0),
+    start: new Date(2024, 10, 16, 10, 0),
+    end: new Date(2024, 10, 16, 11, 0),
   },
 ];
 
@@ -39,6 +39,7 @@ const EventCalendar = () => (
       events={myEventsList}
       startAccessor="start"
       endAccessor="end"
+      onSelectEvent={(event) => console.log(event)}
     />
   </div>
 );
